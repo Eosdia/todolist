c5319bf64d4abc7a4da68ee5b2922c103f897e30
Eosdia <lumimomen@gmail.com>
2023-05-01 22:38:51 +0900

    Countdown update

--- src/TodoHead.js
+++ src/TodoHead.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { MdAlarm } from "react-icons/md";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const TodoHeadBlock = styled.div`
   display: flex;
@@ -19,35 +21,50 @@ const TodoHeadBlock = styled.div`
     justify-content: center;
     align-items: center;
   }
+  h1 {
+    font-size: 70px;
+    margin: 0;
+  }
   h4,
   h5 {
     margin-top: 5px;
     margin-bottom: 5px;
-    margin-left: 10px;
+    font-size: 18px;
   }
 `;
 
 function TodoHead() {
   const today = new Date();
-  const dateString = today.toLocaleDateString("ko-KR", {
-    month: "long",
-    day: "numeric",
-  });
-  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  const year = today.toLocaleDateString("en-US", { year: "numeric" });
+  const month = today.toLocaleDateString("en-US", { month: "long" });
+  const day = today.toLocaleDateString("en-US", { day: "2-digit" });
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  console.log(today.getHours());
   return (
     <TodoHeadBlock>
       <div className="day">
-        <h1>27</h1>
+        <h1>{day}</h1>
         <div className="flex-container ">
-          <h4>April</h4>
-          <h5>{dayName}</h5>
+          <h4>{month}</h4>
+          <h5>{year}</h5>
         </div>
       </div>
       <div className="flex-container">
         <h2>Time Out</h2>
         <div className="time">
           <MdAlarm />
-          16:24:38
+          {`${("0" + (23 - hours)).slice(-2)}:${("0" + (59 - minutes)).slice(
+            -2
+          )}:${("0" + (60 - seconds)).slice(-2)}`}
         </div>
       </div>
     </TodoHeadBlock>
